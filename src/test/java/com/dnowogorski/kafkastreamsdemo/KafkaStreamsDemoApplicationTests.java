a34fa00f429f1e791822afc3f953653da588ed74
dnowogorski <dawid.nowogorski@grandparade.co.uk>
2020-04-08 11:45:19 +0200

    Add project skeleton

---
+++ src/test/java/com/dnowogorski/kafkastreamsdemo/KafkaStreamsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dnowogorski.kafkastreamsdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaStreamsDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
