b6faf2c534818870c0e5839d68ca8e95ba3750db
dnowogorski <nowogorski.dawid@gmail.com>
2020-04-16 12:21:52 +0200

    Add basic consumer and infrastructure

--- src/main/java/com/dnowogorski/kafkastreamsdemo/KafkaStreamsDemoApplication.java
+++ src/main/java/com/dnowogorski/kafkastreamsdemo/KafkaStreamsDemoApplication.java
@@ -1,7 +1,9 @@
 package com.dnowogorski.kafkastreamsdemo;
 
+import org.apache.kafka.streams.kstream.KStream;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class KafkaStreamsDemoApplication {
@@ -10,4 +12,10 @@ public class KafkaStreamsDemoApplication {
         SpringApplication.run(KafkaStreamsDemoApplication.class, args);
     }
 
+    @Bean
+    public java.util.function.Consumer<KStream<String, String>> process() {
+
+        return input ->
+                input.foreach((key, value) -> System.out.println("Key: " + key + " Value: " + value));
+    }
 }
